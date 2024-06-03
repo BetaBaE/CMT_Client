b3d515221ae5caca6b1585ad9d093ef67b5f580b
yassinethefrt <yassinefartassi01@gmail.com>
2024-06-03 15:58:55 +0100

    Enhanced CreateFacture component with additional fields and dynamic fournisseur data
    Added fetching and displaying of designations.
    Introduced state management for advances by fournisseur and their sum.
    Implemented handleOnchangeFournisseur to update advance information based on selected fournisseur.
    Updated form fields to include MontantHT, MontantTVA, and ReferenceAvance.
    Added dynamic display of fournisseur advance summary.
    Improved user experience by ensuring all necessary data is loaded and displayed.

--- src/components/Factures/EditFacture.js
+++ src/components/Factures/EditFacture.js
@@ -13,7 +13,13 @@ import {
   required,
   useGetIdentity,
 } from "react-admin";
-import { getChantier, getFournisseur } from "../Global/getAssets.mjs";
+import {
+  avancesumbyfournisseur,
+  getAvanceByFournisseur,
+  getChantier,
+  getDesignation,
+  getFournisseur,
+} from "../Global/getAssets.mjs";
 
 const useStyles = makeStyles(() => ({
   inputSize: {
@@ -27,6 +33,10 @@ const EditFacture = () => {
 
   const [fournisseurs, setFournisseurs] = useState([]);
   const [chantier, setChantier] = useState([]);
+  const [designation, setDesignation] = useState([]);
+  const [idFounisseur, setIdFounisseur] = useState();
+  const [avanceByFounisseur, setAvanceByFounisseur] = useState([]);
+  const [avanceSumByFounisseur, setAvanceSumByFounisseur] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,6 +45,9 @@ const EditFacture = () => {
         setChantier(chantierData);
         const fournisseurData = await getFournisseur();
         setFournisseurs(fournisseurData);
+        getDesignation().then((data) => {
+          setDesignation(data);
+        });
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -51,12 +64,40 @@ const EditFacture = () => {
     id: item.id,
     name: `${item.id} | ${item.LIBELLE}`,
   }));
+  const designation_choices = designation.map((item) => {
+    let id = item.id;
+    let name = `${item.Designation} | ${Math.round(
+      (item.PourcentageTVA - 1) * 100
+    )}%`;
+    return { id, name };
+  });
+  const referenceAvance_choices = avanceByFounisseur.map((item) => {
+    let id = item.id;
+    // Format the MontantTotal as a currency with two decimal places
+    let montantTotal = parseFloat(item.MontantTotal).toFixed(2);
+
+    // Format the DateAvance as a date string
+    let dateAvance = new Date(item.DateAvance).toLocaleDateString("fr-FR");
+
+    let name = `${item.BonCommande} | ${dateAvance} | ${montantTotal}`;
+    return { id, name };
+  });
   const FactureEditToolbar = (props) => (
     <Toolbar {...props}>
       <SaveButton id="save" />
     </Toolbar>
   );
 
+  function handleOnchangeFournisseur(id) {
+    if (id != "") {
+      getAvanceByFournisseur(id).then((data) => {
+        setAvanceByFounisseur(data);
+      });
+      avancesumbyfournisseur(id).then((data) => {
+        setAvanceSumByFounisseur(data);
+      });
+    }
+  }
   return (
     <Edit>
       <SimpleForm toolbar={<FactureEditToolbar />}>
@@ -87,9 +128,25 @@ const EditFacture = () => {
             <AutocompleteInput
               source="IdFournisseur"
               label="Fournisseur"
-              choices={fournisseurs_choices}
+              id="IdFournisseur"
               className={classes.inputSize}
+              choices={fournisseurs_choices}
+              onChange={(e) => {
+                handleOnchangeFournisseur(e);
+                setIdFounisseur(e);
+              }}
             />
+            {avanceSumByFounisseur.length > 0 && idFounisseur !== "" ? (
+              <span>
+                {`Le fournisseur `}
+                <b>{avanceSumByFounisseur[0].nom}</b>
+                {` a un montant de `}
+                <b>{Number(avanceSumByFounisseur[0].MontantTTC).toFixed(2)}</b>
+                {`  TTC pour les avances.`}
+              </span>
+            ) : (
+              ""
+            )}
           </Grid>
           <Grid item lg={6} md={12} sm={12} xs={12}>
             <DateInput
@@ -106,8 +163,9 @@ const EditFacture = () => {
             />
           </Grid>
           <Grid item lg={6} md={12} sm={12} xs={12}>
-            <TextInput
-              source="ReferenceAvanace"
+            <AutocompleteInput
+              choices={referenceAvance_choices}
+              source="ReferenceAvance"
               className={classes.inputSize}
             />
           </Grid>
@@ -139,32 +197,12 @@ const EditFacture = () => {
             />
           </Grid>
           <Grid item lg={6} md={12} sm={12} xs={12}>
-            <NumberInput
-              source="MontantHT"
-              label="Montant HT"
-              min={0}
-              validate={required("Veuillez entrer un MontantHT")}
-              className={classes.inputSize}
-            />
-          </Grid>
-          <Grid item lg={6} md={12} sm={12} xs={12}>
-            <NumberInput
-              source="MontantTVA"
-              label="Montant TVA"
-              min={0}
-              validate={required("Veuillez entrer un MontantTVA")}
-              className={classes.inputSize}
-            />
-          </Grid>
-          <Grid item lg={6} md={12} sm={12} xs={12}>
             <AutocompleteInput
               source="IdDesignation"
               validate={required("Veuillez entrer une Designation")}
               className={classes.inputSize}
-              choices={[
-                { id: "D1", name: "Designation 1 " },
-                { id: "D2", name: "Designation 1" },
-              ]}
+              label="Designation"
+              choices={designation_choices}
             />
           </Grid>
           <Grid item lg={6} md={12} sm={12} xs={12}>
@@ -197,7 +235,32 @@ const EditFacture = () => {
               defaultValue="Non"
               choices={[
                 { id: "Oui", name: "Oui" },
-                { id: "NON", name: "NON" },
+                { id: "Non", name: "Non" },
+              ]}
+            />
+          </Grid>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <SelectInput
+              source="VerifiyMidelt"
+              className={classes.inputSize}
+              defaultValue="Non"
+              choices={[
+                { id: "Oui", name: "Oui" },
+                { id: "Non", name: "Non" },
+              ]}
+            />
+          </Grid>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <SelectInput
+              className={classes.inputSize}
+              validate={required("Veuillez entrer une categorie")}
+              source="categorie"
+              choices={[
+                {
+                  id: "fourniture de travaux ",
+                  name: "fourniture de travaux  ",
+                },
+                { id: "service", name: "service" },
               ]}
             />
           </Grid>
