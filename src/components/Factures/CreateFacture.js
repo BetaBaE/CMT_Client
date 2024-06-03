3573b532a6f2157c6b74ff5870e0886ea9875c0b
yassinethefrt <yassinefartassi01@gmail.com>
2024-06-03 15:58:20 +0100

    Enhanced CreateFacture component with additional fields and dynamic fournisseur data
    Added fetching and displaying of designations.
    Introduced state management for advances by fournisseur and their sum.
    Implemented handleOnchangeFournisseur to update advance information based on selected fournisseur.
    Updated form fields to include MontantHT, MontantTVA, and ReferenceAvance.
    Added dynamic display of fournisseur advance summary.
    Improved user experience by ensuring all necessary data is loaded and displayed.

--- src/components/Factures/CreateFacture.js
+++ src/components/Factures/CreateFacture.js
@@ -10,10 +10,17 @@ import {
   regex,
   required,
   useGetIdentity,
+  useRecordContext,
 } from "react-admin";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/styles";
-import { getChantier, getFournisseur } from "../Global/getAssets.mjs";
+import {
+  avancesumbyfournisseur,
+  getAvanceByFournisseur,
+  getChantier,
+  getDesignation,
+  getFournisseur,
+} from "../Global/getAssets.mjs";
 
 const useStyles = makeStyles(() => ({
   inputSize: {
@@ -30,20 +37,37 @@ const CreateFacture = () => {
 
   const [fournisseurs, setFournisseurs] = useState([]);
   const [chantier, setChantier] = useState([]);
+  const [designation, setDesignation] = useState([]);
+  const [avanceByFounisseur, setAvanceByFounisseur] = useState([]);
+  const [avanceSumByFounisseur, setAvanceSumByFounisseur] = useState([]);
+  const [idFounisseur, setIdFounisseur] = useState();
 
   useEffect(() => {
     getChantier().then((data) => {
       setChantier(data);
     });
-    getFournisseur()
-      .then((data) => {
-        setFournisseurs(data);
-      })
-      .catch((error) => {
-        console.error("Error in fetching data:", error);
-      });
+    getFournisseur().then((data) => {
+      setFournisseurs(data);
+    });
+
+    getDesignation().then((data) => {
+      setDesignation(data);
+    });
+    // .catch((error) => {
+    //   console.error("Error in fetching data:", error);
+    // });
   }, []);
 
+  function handleOnchangeFournisseur(id) {
+    if (id != "") {
+      getAvanceByFournisseur(id).then((data) => {
+        setAvanceByFounisseur(data);
+      });
+      avancesumbyfournisseur(id).then((data) => {
+        setAvanceSumByFounisseur(data);
+      });
+    }
+  }
   const fournisseurs_choices = fournisseurs.map((item) => {
     let id = item.id;
     let name = `${item.Nom} | ${item.CodeFournisseur}`;
@@ -54,6 +78,26 @@ const CreateFacture = () => {
     let name = `${item.id} | ${item.LIBELLE}`;
     return { id, name };
   });
+
+  const designation_choices = designation.map((item) => {
+    let id = item.id;
+    let name = `${item.Designation} | ${Math.round(
+      (item.PourcentageTVA - 1) * 100
+    )}%`;
+    return { id, name };
+  });
+  const referenceAvance_choices = avanceByFounisseur.map((item) => {
+    let id = item.id;
+    // Format the MontantTotal as a currency with two decimal places
+    let montantTotal = parseFloat(item.MontantTotal).toFixed(2);
+
+    // Format the DateAvance as a date string
+    let dateAvance = new Date(item.DateAvance).toLocaleDateString("fr-FR");
+
+    let name = `${item.BonCommande} | ${dateAvance} | ${montantTotal}`;
+    return { id, name };
+  });
+
   const { isLoading, error } = useGetIdentity();
   if (isLoading) return <>Loading</>;
   if (error) return <>Error</>;
@@ -87,10 +131,27 @@ const CreateFacture = () => {
             <AutocompleteInput
               source="IdFournisseur"
               label="Fournisseur"
+              id="IdFournisseur"
               className={classes.inputSize}
               choices={fournisseurs_choices}
+              onChange={(e) => {
+                handleOnchangeFournisseur(e);
+                setIdFounisseur(e);
+              }}
             />
+            {avanceSumByFounisseur.length > 0 && idFounisseur !== "" ? (
+              <span>
+                {`Le fournisseur `}
+                <b>{avanceSumByFounisseur[0].nom}</b>
+                {` a un montant de `}
+                <b>{Number(avanceSumByFounisseur[0].MontantTTC).toFixed(2)}</b>
+                {`  TTC pour les avances.`}
+              </span>
+            ) : (
+              ""
+            )}
           </Grid>
+
           <Grid item lg={6} md={12} sm={12} xs={12}>
             <DateInput
               source="DateFacture"
@@ -106,8 +167,27 @@ const CreateFacture = () => {
             />
           </Grid>
           <Grid item lg={6} md={12} sm={12} xs={12}>
-            <TextInput
-              source="ReferenceAvanace"
+            <AutocompleteInput
+              choices={referenceAvance_choices}
+              source="ReferenceAvance"
+              className={classes.inputSize}
+            />
+          </Grid>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <NumberInput
+              source="MontantHT"
+              label="Montant HT"
+              min={0}
+              validate={required("Veuillez entrer un MontantHT")}
+              className={classes.inputSize}
+            />
+          </Grid>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <NumberInput
+              source="MontantTVA"
+              label="Montant TVA"
+              min={0}
+              validate={required("Veuillez entrer un MontantTVA")}
               className={classes.inputSize}
             />
           </Grid>
@@ -125,10 +205,8 @@ const CreateFacture = () => {
               className={classes.inputSize}
               validate={required("Veuillez entrer une Designation")}
               source="IdDesignation"
-              choices={[
-                { id: "D1", name: "Designation 1 " },
-                { id: "D2", name: "Designation 1" },
-              ]}
+              label="Designation"
+              choices={designation_choices}
             />
           </Grid>
           <Grid item lg={6} md={12} sm={12} xs={12}>
@@ -145,16 +223,6 @@ const CreateFacture = () => {
               min={0}
               validate={required("Veuillez entrer un NetAPayer")}
               className={classes.inputSize}
-            />
-          </Grid>
-          <Grid item lg={6} md={12} sm={12} xs={12}>
-            <DateInput source="DateTransfer" className={classes.inputSize} />
-          </Grid>
-          <Grid item lg={6} md={12} sm={12} xs={12}>
-            <NumberInput
-              source="Acompte"
-              className={classes.inputSize}
-              min={0}
             />
           </Grid>
           <Grid item lg={6} md={12} sm={12} xs={12}>
